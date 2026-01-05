9f9a91e48f1e22dcd8227dbd1337d3c67ecd004b
Apple <apple@Apples-MacBook-Air.local>
2026-01-05 21:39:08 +0530

    first code that explain the flex

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      {/* <div className="bg-blue-200"> helllo suraj.</div>
+      <div
+        className="flex
+flex-row-reverse"
+      >
+        <div>
+          <button className=" pt-1 pb-1 px-3 py-3 m-2 border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 rounded-3xl border-1">
+            Message
+          </button>
+        </div>
+        <div>
+          <button className=" pt-1 pb-1 px-3 py-3 m-2 border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 rounded-3xl border-1">
+            Message
+          </button>
+        </div>
+        <div>
+          <button className=" pt-1 pb-1 px-3 py-3 m-2 border-purple-200 text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 rounded-3xl border-1">
+            Message */}
+      {/* </button>
+        </div>
+      </div> */}
+      <div class="flex flex-row">
+        <div class="basis-64">01</div>
+        <div class="basis-64">02</div>
+        <div class="basis-128">03</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
